6016cdc5c74d1054d93a6051f232b0e7b99fe2d1
marah-jaradat <marahjaradat97@gmail.com>
2022-02-11 23:48:15 +0200

    ading comments and update'

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
 "use strict";
 
-//
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 3002;
+// const Data = require("package.json");
+// we go undefined becausewe insert object as json inside body, so we have to parse data
+app.use(express.json());
+//Routs
 
-const Data = require("./data.json");
-const PORT = 4012;
+app.get("/", homeHandler);
+app.get("/favorite", favHandler);
+app.get("/user/:id", idHandler);
+app.post("/info", infoHandler);
 
-//
-app.get("/", function (req, res) {
-  return res.status(200).send(movie);
-});
-
-app.get("/favorite", function (req, res) {
+// Functions
+// http://localhost:3000/
+function homeHandler(req, res) {
+  res.send("Hello there");
+  // return res.status(200).send(movie);
+}
+// http://localhost:3000/favorite
+function favHandler(req, res) {
+  // http://localhost:3000/favorite?name=marah
+  // console.log(req.query) as object, query is a keyword for the request
   return res.status(200).send("Welcome to Favorite Page");
-});
+}
+// http://localhost:3000/user/:id
+// req.params:is part of the req
+function idHandler(req, res) {
+  console.log(req.params);
+  res.send("I'm there");
+}
+// req.body:is part of the req
+function infoHandler(req, res) {
+  console.log(req.body);
+  res.send("I'm the body");
+}
 
 // Constructor
 function Movie(title, poster_path, overview) {
